05707093aeb10f896777ec0df34ccb4172c5d996
phantazeeer <shturmovik729@gmail.com>
2026-02-14 21:09:45 +0300

    feat(db): creating tables from python script

---
+++ app/db/models/user.py
@@ -0,0 +1,13 @@
+from app.db.database import get_session
+
+
+async def create_user_table():
+    conn = await get_session()
+    await conn.execute("""CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    balance INTEGER NOT NULL DEFAULT 0,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    goal TEXT,
+    created_at TEXT);""")
