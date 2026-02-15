cf3f4c0113efd30fcf61412754e0cb816738c04a
phantazeeer <shturmovik729@gmail.com>
2026-02-15 16:49:15 +0300

    feat(db): added sql trace

--- app/db/database.py
+++ app/db/database.py
@@ -3,4 +3,7 @@ from aiosqlite import connect
 
 async def get_session(path: str = './app/db/database.db'):
     conn = await connect(path)
+    settings = True # TODO: добавить переменные окружения
+    if settings:
+        await conn.set_trace_callback(print)
     return conn
