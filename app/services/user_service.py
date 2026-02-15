84aba30f7c92facf65a9f0d2945080a8cbaeb52b
phantazeeer <shturmovik729@gmail.com>
2026-02-15 16:50:11 +0300

    feat(db): added user_service with sql code

---
+++ app/services/user_service.py
@@ -0,0 +1,36 @@
+from app.db.database import get_session
+from app.utils import get_password_hash
+
+
+class UserService:
+    async def init_session(self):
+        self.conn = await get_session()
+
+    async def add_user(self, username: str, email: str, password: str):
+        await self.conn.execute("""INSERT INTO user (username, email, password) VALUES (?, ?, ?);""",
+                                (username, email, get_password_hash(password)))
+        await self.conn.commit()
+
+    async def get_user_by(self, email: str | None = None, id: int | None = None):
+        if email:
+            cursor = await self.conn.execute("""SELECT * FROM user WHERE email = ?""", (email,))
+        elif id:
+            cursor = await self.conn.execute("""SELECT * FROM user WHERE id = ?""", (id,))
+        else:
+            raise Exception('incorrect using get_user_by')
+        res = await cursor.fetchone()
+        return res
+
+    async def delete_user(self, id: int):
+        await self.conn.execute("""DELETE FROM user WHERE id = ?""", (id,))
+        await self.conn.commit()
+
+    async def update_goal(self, id: int, goal: str):
+        await self.conn.execute("""UPDATE user SET goal = ? WHERE id = ?""", (goal, id))
+        await self.conn.commit()
+
+
+async def test_class():
+    service = UserService()
+    await service.init_session()
+    await service.update_goal(id=2, goal='FFF:123')
