664e22b4b0ebbb13658c7f776ce3b8c707e05d22
phantazeeer <shturmovik729@gmail.com>
2026-02-21 16:48:16 +0300

    feat(api): added user update scheme

--- app/api/schemas/user.py
+++ app/api/schemas/user.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, model_validator, field_validator
+from types import NoneType
 
 
 class UserRegister(BaseModel):
@@ -10,3 +11,20 @@ class UserRegister(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class UserChange(BaseModel):
+    balance: int | None = None
+    username: str | None = None
+    email: EmailStr | None = None
+    goal_name: str | None = None
+    goal_value: int | None = None
+
+    @model_validator(mode='after')
+    def clean_model(self):
+        """Удаляет поля со значениями None"""
+        attrs = self.__dict__.copy()
+        for i in attrs.keys():
+            if isinstance(attrs[i], NoneType):
+                self.__delattr__(i)
+        return self
