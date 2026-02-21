d7503b9083527542c7215c211d5e942ad27eec2f
phantazeeer <shturmovik729@gmail.com>
2026-02-21 15:03:53 +0300

    feat(service): added login validation

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,7 @@
 from app.db.database import get_session
-from app.utils import get_password_hash
+from app.utils import get_password_hash, verify_password, create_token
 from datetime import datetime, timezone
+from fastapi import HTTPException
 
 
 class UserService:
@@ -14,9 +15,9 @@ class UserService:
 
     async def get_user_by(self, email: str | None = None, id: int | None = None):
         if email:
-            cursor = await self.conn.execute("""SELECT * FROM user WHERE email = ?""", (email,))
+            cursor = await self.conn.execute("""SELECT balance, username, email, goal, created_at FROM user WHERE email = ?""", (email,))
         elif id:
-            cursor = await self.conn.execute("""SELECT * FROM user WHERE id = ?""", (id,))
+            cursor = await self.conn.execute("""SELECT balance, username, email, goal, created_at FROM user WHERE id = ?""", (id,))
         else:
             raise Exception('incorrect using get_user_by')
         res = await cursor.fetchone()
@@ -29,3 +30,14 @@ class UserService:
     async def update_goal(self, id: int, goal: str):
         await self.conn.execute("""UPDATE user SET goal = ? WHERE id = ?""", (goal, id))
         await self.conn.commit()
+
+
+    async def login(self, email: str, password: str) -> str:
+        user = await (await self.conn.execute("""SELECT * FROM user WHERE email = ?""", (email,))).fetchone()
+        if not user:
+            raise HTTPException(404, "Пользователь не найден")
+        if not verify_password(password, user[4]):
+            raise HTTPException(400, "Неправильный пароль")
+        else:
+            access_token = create_token(user[0])
+            return access_token
