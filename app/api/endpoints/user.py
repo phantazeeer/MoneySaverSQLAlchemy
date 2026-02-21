b2216d4e231da59f63031a51dc29a441a5ca303b
phantazeeer <shturmovik729@gmail.com>
2026-02-21 15:04:28 +0300

    feat(api): ended working with token

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Response
 
+from app.api.schemas import UserLogin
 from app.services import UserService
 from app.api.schemas import UserRegister
+from app.utils import get_jwt_payload
 
 router = APIRouter(prefix='/user', tags=['Working with user'])
+
 
 async def get_service():
     service = UserService()
@@ -11,12 +14,27 @@ async def get_service():
     return service
 
 
-@router.post('/test')
-async def test(service: UserService = Depends(get_service)):
-    user = await service.get_user_by(id=1)
-    return user
+@router.post('/me')
+async def me(user_id: int = Depends(get_jwt_payload), service: UserService = Depends(get_service)):
+    res = await service.get_user_by(id=user_id)
+    return res
+
 
 @router.post('/register', status_code=status.HTTP_201_CREATED)
-async def register(user: UserRegister, service: UserService = Depends(get_service)):
-    await service.add_user(user.username, str(user.email), user.password)
+async def register(create: UserRegister, service: UserService = Depends(get_service)):
+    await service.add_user(create.username, str(create.email), create.password)
 
+
+@router.post('/login')
+async def login(user: UserLogin, service: UserService = Depends(get_service)):
+    token = await service.login(str(user.email), user.password)
+    res = Response("OK", status_code=200)
+    res.set_cookie('Authorization', token, httponly=True)
+    return res
+
+
+@router.post('/logout')
+async def logout():
+    res = Response("OK", status_code=200)
+    res.delete_cookie('Authorization')
+    return res
