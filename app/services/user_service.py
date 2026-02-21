2a208a935a688f02f6b81daa8c6ed9d4cacd1cf2
phantazeeer <shturmovik729@gmail.com>
2026-02-21 16:47:54 +0300

    feat(service): added user update

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,6 +2,7 @@ from app.db.database import get_session
 from app.utils import get_password_hash, verify_password, create_token
 from datetime import datetime, timezone
 from fastapi import HTTPException
+from sqlite3 import IntegrityError
 
 
 class UserService:
@@ -15,9 +16,11 @@ class UserService:
 
     async def get_user_by(self, email: str | None = None, id: int | None = None):
         if email:
-            cursor = await self.conn.execute("""SELECT balance, username, email, goal, created_at FROM user WHERE email = ?""", (email,))
+            cursor = await self.conn.execute(
+                """SELECT balance, username, email, goal, created_at FROM user WHERE email = ?""", (email,))
         elif id:
-            cursor = await self.conn.execute("""SELECT balance, username, email, goal, created_at FROM user WHERE id = ?""", (id,))
+            cursor = await self.conn.execute(
+                """SELECT balance, username, email, goal, created_at FROM user WHERE id = ?""", (id,))
         else:
             raise Exception('incorrect using get_user_by')
         res = await cursor.fetchone()
@@ -27,10 +30,19 @@ class UserService:
         await self.conn.execute("""DELETE FROM user WHERE id = ?""", (id,))
         await self.conn.commit()
 
-    async def update_goal(self, id: int, goal: str):
-        await self.conn.execute("""UPDATE user SET goal = ? WHERE id = ?""", (goal, id))
-        await self.conn.commit()
-
+    async def update_user(self, id: int, **kwargs):
+        base = """UPDATE user \nSET """
+        values = tuple(kwargs.values()) + (id,)
+        for i in kwargs.keys():
+            base += i + " = ?, "
+        base = base[:-2]
+        base += '\nWHERE id = ?'
+        try:
+            await self.conn.execute(base, values)
+            await self.conn.commit()
+        except IntegrityError as err:
+            if "user.email" in str(err):
+                raise HTTPException(400, "Введите другую почту")
 
     async def login(self, email: str, password: str) -> str:
         user = await (await self.conn.execute("""SELECT * FROM user WHERE email = ?""", (email,))).fetchone()
