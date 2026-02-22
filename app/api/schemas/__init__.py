476edc3bb1834b033a038b36ea60d832df396fc5
phantazeeer <shturmovik729@gmail.com>
2026-02-22 14:47:49 +0300

    feat(validation): added record model

--- app/api/schemas/__init__.py
+++ app/api/schemas/__init__.py
@@ -1,2 +1,3 @@
 from .user import UserRegister, UserLogin, UserChange
 from .costs_and_earnings import AddRecord, ChangeRecord
+from .costs_and_earnings import AddRecord, ChangeRecord, Record
--- app/api/schemas/costs_and_earnings.py
+++ app/api/schemas/costs_and_earnings.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Literal
+from datetime import datetime
 
 
 class AddRecord(BaseModel):
@@ -13,3 +14,11 @@ class ChangeRecord(BaseModel):
     operation_type: Literal[0, 1] | None = None
     value: int | None = None
     comment: str | None = None
+
+class Record(BaseModel):
+    id: int
+    user_id: int
+    operation_type: bool
+    value: int
+    comment: str | None = None
+    created_at: datetime
