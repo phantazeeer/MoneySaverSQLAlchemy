f48b664d2ee98729906fc8d467a5466cdd257969
phantazeeer <shturmovik729@gmail.com>
2026-02-22 14:48:34 +0300

    feat(validation): added validation on ce_service

--- app/services/costs_and_earnings_service.py
+++ app/services/costs_and_earnings_service.py
@@ -1,15 +1,16 @@
 from fastapi import HTTPException
 
 from app.db.database import get_session
+from app.api.schemas import Record
 from datetime import datetime, timezone
 from types import NoneType
 
 
 class CostsAndEarningsService:
-    async def init_session(self):
+    async def init_session(self) -> None:
         self.conn = await get_session()
 
-    async def add_record(self, user_id: int, operation_type: int, value: int, comment: str | None = None):
+    async def add_record(self, user_id: int, operation_type: int, value: int, comment: str | None = None) -> None:
         await self.conn.execute(
             """INSERT INTO costs_and_earnings (user_id, operation_type, value, comment, created_at) VALUES (?, ?, ?, ?, ?)""",
             (user_id, operation_type, value, comment, datetime.now(timezone.utc)))
@@ -17,7 +18,7 @@ class CostsAndEarningsService:
                                 ((-1) ** operation_type * value, user_id))
         await self.conn.commit()
 
-    async def delete_record(self, id: int, user_id: int):
+    async def delete_record(self, id: int, user_id: int) -> None:
         record = await (await self.conn.execute("""SELECT * FROM costs_and_earnings WHERE id = ?""", (id,))).fetchone()
         if isinstance(record, NoneType):
             raise HTTPException(404, "Такой записи нет")
@@ -29,23 +30,37 @@ class CostsAndEarningsService:
         await self.conn.execute("""DELETE FROM costs_and_earnings WHERE id = ?""", (id,))
         await self.conn.commit()
 
-    async def get_records_by(self, id: int | None = None, user_id: int | None = None):
+    async def get_records_by(self, id: int | None = None, user_id: int | None = None) -> Record | list[Record]:
         if not isinstance(id, NoneType) and not isinstance(user_id, NoneType):
             cursor = await self.conn.execute("""SELECT * FROM costs_and_earnings WHERE id = ?""", (id,))
-            res = await cursor.fetchone()
-            if isinstance(res, NoneType):
+            record = await cursor.fetchone()
+            if isinstance(record, NoneType):
                 raise HTTPException(404, "Такой записи нет")
-            if not res[1] == user_id:
+            if not record[1] == user_id:
                 raise HTTPException(400, "Пользователь не владелец записи")
+            res = Record(id=record[0],
+                         user_id=record[1],
+                         operation_type=record[2],
+                         value=record[3],
+                         comment=record[4],
+                         created_at=record[5])
         elif not isinstance(user_id, NoneType):
             cursor = await self.conn.execute("""SELECT * FROM costs_and_earnings WHERE user_id = ?""", (user_id,))
-            res = await cursor.fetchall()
+            records = await cursor.fetchall()
+            res = list()
+            for record in records:
+                res.append(Record(id=record[0],
+                                  user_id=record[1],
+                                  operation_type=record[2],
+                                  value=record[3],
+                                  comment=record[4],
+                                  created_at=record[5]))
         else:
             raise HTTPException(400, 'incorrect using get_records_by')
         return res
 
     async def update_record(self, id: int, user_id: int, operation_type: int | None = None, value: int | None = None,
-                            comment: str | None = None):
+                            comment: str | None = None) -> None:
         record = await (await self.conn.execute("""SELECT * FROM costs_and_earnings WHERE id = ?""", (id,))).fetchone()
         if isinstance(record, NoneType):
             raise HTTPException(404, "Такой записи нет")
