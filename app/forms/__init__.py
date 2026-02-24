a91881a7d1f8ce2c7c46b6447b021c65f4f36673
phantazeeer <shturmovik729@gmail.com>
2026-02-25 00:50:29 +0300

    feat(forms): added date from for graphic

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .fast_add_record_form import FastAddRecordForm
 from .login_form import LoginForm
 from .register_form import RegisterForm
 from .change_record_form import ChangeRecordForm
+from .get_statistics import ChooseDateForm
