48f55bfd12ee7696651cdfea2c03646b3f34bb82
phantazeeer <shturmovik729@gmail.com>
2026-03-05 23:05:18 +0300

    chore(db): removed TODO

--- app/db/database.py
+++ app/db/database.py
@@ -3,7 +3,7 @@ from aiosqlite import connect, Connection
 
 async def get_session(path: str = './app/db/database.db') -> Connection:
     conn = await connect(path)
-    settings = True # TODO: добавить переменные окружения
+    settings = True
     if settings:
         await conn.set_trace_callback(print)
     await conn.execute("PRAGMA foreign_keys = ON")
