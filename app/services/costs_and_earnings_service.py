62b46f70fa7a98207d16517542fd35ac1f622ccd
phantazeeer <shturmovik729@gmail.com>
2026-03-14 22:36:42 +0300

    feat(services): added DTO

--- app/services/costs_and_earnings_service.py
+++ app/services/costs_and_earnings_service.py
@@ -21,92 +21,92 @@ class CostsAndEarningsService:
         await self.cedao.init_conn()
 
     async def add_record(self, user_id: int, operation_type: str, value: int, comment: str | None = None) -> None:
-        await self.cedao.create(user_id=user_id, operation_type=int(operation_type), value=value)
+        await self.cedao.create(user_id=user_id, operation_type=int(operation_type), value=value, comment=comment)
         await self.userdao.do_raw_sql("""UPDATE user SET balance = balance + ? WHERE id = ?""",
                                       ((-1) ** int(operation_type) * value, user_id))
 
     async def delete_record(self, id: int, user_id: int) -> None:
-        records = await self.cedao.get_by(id=id, mode="AND")
+        records = await self.cedao.get_by(id=id)
         if not records:
             raise HTTPException(404, "Такой записи нет")
         record = records[0]
-        if not record[1] == user_id:
+        if record.user_id != user_id:
             raise HTTPException(400, "Пользователь не является владельцем маршрута")
-        user = (await self.userdao.get_by(mode="AND", id=user_id))[0]
+        user = (await self.userdao.get_by(id=user_id))[0]
         await self.userdao.do_raw_sql("""UPDATE user SET balance = balance - ? WHERE id = ?""",
-                                      ((-1) ** record[2] * record[3], user[0]))
+                                      ((-1) ** record.operation_type * record.value, user.id))
         await self.cedao.delete(id=id)
 
     async def get_records_by(self, id: int | None = None, user_id: int | None = None) -> Record | list[Record]:
         if not isinstance(id, NoneType) and not isinstance(user_id, NoneType):
-            records = await self.cedao.get_by(id=id, mode="AND")
+            records = await self.cedao.get_by(id=id)
             if not records:
                 raise HTTPException(404, "Такой записи нет")
             record = records[0]
-            if not record[1] == user_id:
+            if not record.user_id == user_id:
                 raise HTTPException(400, "Пользователь не владелец записи")
-            res = Record(id=record[0],
-                         user_id=record[1],
-                         operation_type=record[2],
-                         value=record[3],
-                         comment=record[4],
-                         created_at=record[5])
+            res = Record(id=record.id,
+                         user_id=record.user_id,
+                         operation_type=record.operation_type,
+                         value=record.value,
+                         comment=record.comment,
+                         created_at=record.created_at)
         elif not isinstance(user_id, NoneType):
-            records = await self.cedao.get_by(mode="AND", user_id=user_id)
+            records = await self.cedao.get_by(user_id=user_id)
             res = list()
             for record in records:
-                res.append(Record(id=record[0],
-                                  user_id=record[1],
-                                  operation_type=record[2],
-                                  value=record[3],
-                                  comment=record[4],
-                                  created_at=record[5]))
+                res.append(Record(id=record.id,
+                                  user_id=record.user_id,
+                                  operation_type=record.operation_type,
+                                  value=record.value,
+                                  comment=record.comment,
+                                  created_at=record.created_at))
         else:
             raise HTTPException(400, 'incorrect using get_records_by')
         return res
 
     async def update_record(self, id: int, user_id: int, operation_type: int | None = None, value: int | None = None,
                             comment: str | None = None) -> None:
-        records = await self.cedao.get_by(mode="AND", id=id)
+        records = await self.cedao.get_by(id=id)
         if not records:
             raise HTTPException(404, "Такой записи нет")
         record = records[0]
-        if not record[1] == user_id:
+        if not record.user_id == user_id:
             raise HTTPException(400, "Пользователь не владелец записи")
         if isinstance(operation_type, NoneType):
-            operation_type = record[2]
+            operation_type = record.operation_type
         if isinstance(value, NoneType):
-            value = record[3]
+            value = record.value
         if isinstance(comment, NoneType):
-            comment = record[4]
-        if not (operation_type == record[2] and value == record[3]):
-            if operation_type == record[2]:
-                balance_change = (value - record[3]) * (-1) ** operation_type
+            comment = record.comment
+        if not (operation_type == record.operation_type and value == record.value):
+            if operation_type == record.operation_type:
+                balance_change = (value - record.value) * (-1) ** operation_type
             else:
-                balance_change = (value + record[3]) * (-1) ** operation_type
-            user = (await self.userdao.get_by(mode="AND", id=record[1]))[0]
+                balance_change = (value + record.value) * (-1) ** operation_type
+            user = (await self.userdao.get_by(id=record.user_id))[0]
             await self.userdao.do_raw_sql("""UPDATE user SET balance = balance + ? WHERE id = ?""",
-                                          (balance_change, user[0]))
+                                          (balance_change, user.id))
         await self.cedao.update(id=id, operation_type=operation_type, value=value, comment=comment)
 
     async def user_costs_or_earnings(self, user_id: int, filter: Literal["costs", "earnings"]) -> list[Record]:
         op_type = 0 if filter == "earnings" else 1
-        records = await self.cedao.get_by(mode="AND", user_id=user_id, operation_type=op_type)
+        records = await self.cedao.get_by(user_id=user_id, operation_type=op_type)
         res = list()
         for record in records:
-            res.append(Record(id=record[0],
-                              user_id=record[1],
-                              operation_type=record[2],
-                              value=record[3],
-                              comment=record[4],
-                              created_at=record[5]))
+            res.append(Record(id=record.id,
+                              user_id=record.user_id,
+                              operation_type=record.operation_type,
+                              value=record.value,
+                              comment=record.comment,
+                              created_at=record.created_at))
         return res
 
     async def create_graphics(self, period: tuple[datetime, datetime], user_id: int):
         record = await self.cedao.do_raw_sql(
             """SELECT * FROM costs_and_earnings WHERE (created_at BETWEEN ? AND ?) AND user_id = ? ORDER BY created_at""",
             (period[0].replace(tzinfo=timezone.utc), period[1].replace(tzinfo=timezone.utc), user_id))
-        balance = (await self.userdao.get_by(mode="AND", id=user_id))[0][0]
+        balance = (await self.userdao.get_by(id=user_id))[0].balance
         records = await record.fetchall()
         if not records:
             return None
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,7 +1,5 @@
-from app.db.database import get_session
 from app.utils import get_password_hash, verify_password, create_token
 from app.api.schemas import User
-from datetime import datetime, timezone
 from fastapi import HTTPException
 from sqlite3 import IntegrityError
 from app.db.DAO import UserDAO
@@ -17,18 +15,17 @@ class UserService:
 
     async def get_user_by(self, email: str | None = None, id: int | None = None) -> User:
         if email:
-            row = (await self.user_dao.get_by(mode="AND", email=email))[0]
+            row = (await self.user_dao.get_by(email=email))[0]
         elif id:
-            row = (await self.user_dao.get_by(mode="AND", id=id))[0]
+            row = (await self.user_dao.get_by(id=id))[0]
         else:
             raise HTTPException(400, 'incorrect using get_user_by')
-        goal_value = row[5][row[5].index('#') + 1:]
-        user = User(balance=row[1],
-                    username=row[2],
-                    email=row[3],
-                    goal_name=row[5][:row[5].index('#')],
-                    goal_value= int(goal_value) if goal_value else 0,  # доделать страницу отображения пользователя
-                    created_at=row[6])
+        user = User(balance=row.balance,
+                    username=row.username,
+                    email=row.email,
+                    goal_name=row.goal_name,
+                    goal_value= int(row.goal_value) if row.goal_value else 0,  # доделать страницу отображения пользователя
+                    created_at=row.created_at)
         return user
 
     async def delete_user(self, id: int) -> None:
@@ -36,19 +33,19 @@ class UserService:
 
     async def update_user(self, id: int, **kwargs) -> None:
         try:
+            print("123123123123213")
             await self.user_dao.update(id=id, **kwargs)
         except IntegrityError as err:
             if "user.email" in str(err):
                 raise HTTPException(400, "Введите другую почту")
 
     async def login(self, email: str, password: str) -> str:
-        users = await self.user_dao.get_by(mode="AND", email=email)
+        users = await self.user_dao.get_by(email=email)
         if len(users) == 0:
             raise HTTPException(404, "Пользователь не найден")
         user = users[0]
-        print(user)
-        if not verify_password(password, user[4]):
+        if not verify_password(password, user.password):
             raise HTTPException(400, "Неправильный пароль")
         else:
-            access_token = create_token(user[0])
+            access_token = create_token(user.id)
             return access_token
