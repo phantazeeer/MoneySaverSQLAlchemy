4c44a9fb6d8964f27f55f8d5a42c0b70fea6be56
phantazeeer <shturmovik729@gmail.com>
2026-03-14 22:37:35 +0300

    fix(db_util): fixed for normalized db

--- db_util.py
+++ db_util.py
@@ -4,6 +4,7 @@ from app.services.user_service import UserService
 from app.services.costs_and_earnings_service import CostsAndEarningsService
 from app.db.models.user import create_user_table
 from app.db.models.costs_and_earnings import create_costs_and_earnings_table
+from app.db.DAO import SQLiteCostsAndEarningsDAO as SQLiteCEDAO, SQLiteUserDAO
 
 
 async def create_all_tables():
@@ -19,7 +20,7 @@ async def drop_all_tables():
 
 async def fill_user_table():
     service = UserService()
-    await service.init_session()
+    await service.init_session(SQLiteUserDAO())
     await service.add_user('Максим Струнников', 'ms@gmail.com', '123')
     await service.add_user('Николас Сенченков', 'ns@gmail.com', '123')
     await service.add_user('Дэнис Качалин', 'dk@gmail.com', '123')
@@ -29,7 +30,7 @@ async def fill_user_table():
 
 async def fill_costs_and_earnings_table():
     service = CostsAndEarningsService()
-    await service.init_session()
+    await service.init_session(SQLiteUserDAO(), SQLiteCEDAO())
     await service.add_record(1, 1, 500)
     await service.add_record(1, 0, 1500)
     await service.add_record(1, 1, 300, "Купил пирожок в столовой")
