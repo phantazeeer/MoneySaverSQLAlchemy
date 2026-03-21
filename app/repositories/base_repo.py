1cc59ca54ce9738bfeae3aae6fc8cd287d3a71e8
phantazeeer <shturmovik729@gmail.com>
2026-03-21 22:52:51 +0300

    feat(db): repositories

---
+++ app/repositories/base_repo.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+from app.db.models import Base
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy import insert, delete
+
+class AbstractRepository(ABC):
+    @abstractmethod
+    async def get_one(self, **kwargs):
+        pass
+
+    @abstractmethod
+    async def add_one(self, **kwargs):
+        pass
+
+    @abstractmethod
+    async def delete_by_id(self, instance: Base):
+        pass
+
+    @abstractmethod
+    async def get_list_by(self, **kwargs):
+        pass
+
+class BasicRepository(AbstractRepository):
+    model = None
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_one(self, **kwargs):
+        res = await self.session.execute(select(self.model).filter_by(**kwargs))
+        return res.scalar_one()
+
+    async def add_one(self, **kwargs):
+        stmt = insert(self.model).values(**kwargs).returning(self.model)
+        res = await self.session.execute(stmt)
+        return res.scalar_one()
+
+    async def delete_by_id(self, id: int):
+        stmt = delete(self.model).where(self.model.id == id).returning(self.model)
+        res = await self.session.execute(stmt)
+        res = res.scalar_one()
+        return res
+
+    async def get_list_by(self, **kwargs):
+        res = await self.session.execute(select(self.model).filter_by(**kwargs))
+        return res.scalars()
