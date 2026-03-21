351e36a532c5b44bbff66cc694fd849d325f2283
phantazeeer <shturmovik729@gmail.com>
2026-03-21 22:52:28 +0300

    feat(db): sqlalchemy tools

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,11 @@
-from aiosqlite import connect, Connection
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.config import settings
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
+engine = create_async_engine(settings.database_url)
+print(f"Connecting '{settings.database_url}'")
+session_factory = async_sessionmaker(engine, class_=AsyncSession)
 
-async def get_session(path: str = './app/db/database.db') -> Connection:
-    conn = await connect(path)
-    settings = True
-    if settings:
-        await conn.set_trace_callback(print)
-    await conn.execute("PRAGMA foreign_keys = ON")
-    return conn
+async def get_async_session():
+    async with session_factory() as session:
+        yield session
