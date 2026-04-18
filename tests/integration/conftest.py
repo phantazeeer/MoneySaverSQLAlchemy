aa8690f7937e30403b36ac478353381fbeac47ed
phantazeeer <shturmovik729@gmail.com>
2026-04-18 20:03:21 +0300

    feat(tests): added user_repo tests

---
+++ tests/integration/conftest.py
@@ -0,0 +1,34 @@
+from typing import AsyncGenerator
+
+import pytest
+import os
+from app.db.models import *
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+DB_PATH = "./db_for_test"
+DB_URL = "sqlite+aiosqlite:///" + DB_PATH
+
+engine = create_async_engine(DB_URL)
+_session_maker = async_sessionmaker(engine, class_=AsyncSession)
+
+async def get_session() -> AsyncGenerator[AsyncSession]:
+    async with _session_maker() as _session:
+        yield _session
+
+@pytest_asyncio.fixture(scope="session")
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+
+@pytest_asyncio.fixture(scope="function")
+async def ready_session(create_tables):
+    sess_gen = get_session()
+    session = await anext(sess_gen)
+    yield session
+    await session.close()
+    await sess_gen.aclose()
