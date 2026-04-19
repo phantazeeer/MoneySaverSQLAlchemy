aade1858392f375ac08dfacc588cc170dd3b101f
phantazeeer <shturmovik729@gmail.com>
2026-04-19 11:44:56 +0300

    refactored(api)

--- app/api/endpoints/costs_and_earnings.py
+++ app/api/endpoints/costs_and_earnings.py
@@ -19,7 +19,7 @@ async def add(record: Annotated[AddRecord, Form()], user_id: int = Depends(get_j
 
 @router.get('/{id}')
 async def get(id: Annotated[int, Path()], user_id: int = Depends(get_jwt_payload),
-              service: CEService = Depends(get_service)) -> Record:
+              service: CEService = Depends(get_service)) -> Record | None:
     try:
         return await service.get_records_by(id=id, user_id=user_id)
     except ValueError as err:
@@ -35,19 +35,21 @@ async def update(id: Annotated[int, Path()], changes: Annotated[ChangeRecord, Fo
                  user_id: int = Depends(get_jwt_payload),
                  service: CEService = Depends(get_service)) -> str:
     try:
-        await service.update_record(id, user_id, int(changes.operation_type), changes.value, changes.comment)
+        await service.update_record(id, user_id, changes.operation_type, changes.value, changes.comment)
         return "OK"
     except ValueError as err:
         if str(err) == "Пользователь не является владельцем записи":
             raise HTTPException(403, "Пользователь не является владельцем записи")
+        raise err
     except Exception as err:
         if str(err) == "Запись не найдена":
             raise HTTPException(404, "Запись не найдена")
+        raise err
 
 
 @router.delete('/{id}')
 async def delete(id: int, user_id: int = Depends(get_jwt_payload),
-                 service: CEService = Depends(get_service)) -> str:
+                 service: CEService = Depends(get_service)) -> str | None:
     try:
         await service.delete_record(id=id, user_id=user_id)
         return "OK"
