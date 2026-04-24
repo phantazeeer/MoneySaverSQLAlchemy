af846bf07daf890dde30a43dcafa59c1593ab5f7
phantazeeer <shturmovik729@gmail.com>
2026-04-24 13:24:23 +0300

    feat(templates): added comment field for fast adding records

--- app/forms/fast_add_record_form.py
+++ app/forms/fast_add_record_form.py
@@ -1,8 +1,9 @@
-from wtforms import Form, IntegerField, SelectField, SubmitField
+from wtforms import Form, IntegerField, SelectField, SubmitField, StringField
 from wtforms.validators import DataRequired
 
 
 class FastAddRecordForm(Form):
     value = IntegerField("Введите значение", validators=[DataRequired()])
     operation_type = SelectField("Введите тип записи", choices=[(0, "доход"), (1, "расход")], validators=[DataRequired()], default=0)
+    comment = StringField("Введите комментарий к записи")
     submit = SubmitField("Добавить запись")
