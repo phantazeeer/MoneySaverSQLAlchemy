8b065ba1fcf1ef630ab2bd914a0809a80d20e6a2
phantazeeer <shturmovik729@gmail.com>
2026-04-30 19:01:10 +0300

    fix(categories table): added unique constraint

--- app/db/models/categories.py
+++ app/db/models/categories.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 class Category(Base):
     __tablename__ = "categories"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
 
     costs_and_earnings: Mapped["CostsAndEarnings"] = relationship(back_populates="category")
