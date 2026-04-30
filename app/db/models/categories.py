1b0e813db3387cc7616cdf78c701efb7c6b8f3c6
phantazeeer <shturmovik729@gmail.com>
2026-04-30 22:02:20 +0300

    fix(categories): fixed relations :)

--- app/db/models/categories.py
+++ app/db/models/categories.py
@@ -1,3 +1,5 @@
+from sqlalchemy import ForeignKey, UniqueConstraint
+
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -5,6 +7,11 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 class Category(Base):
     __tablename__ = "categories"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(nullable=False, unique=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
+
+    __table_args__ = (
+        UniqueConstraint('user_id', 'name', name='ix_category_user_name'),
+    )
 
     costs_and_earnings: Mapped["CostsAndEarnings"] = relationship(back_populates="category")
--- app/repositories/category_repo.py
+++ app/repositories/category_repo.py
@@ -1,6 +1,21 @@
+from tests.integration.repositories.test_costs_and_earnings_repo import user_id
 from .base_repo import BasicRepository
 from app.db.models import Category
+from sqlalchemy.exc import IntegrityError
+from app.utils.logger import get_logger
+
+log = get_logger(__name__)
 
 
 class CategoryRepository(BasicRepository):
     model = Category
+
+    async def add_one(self, name: str, user_id: int) -> Category:
+        try:
+            return await super().add_one(name=name, user_id=user_id) # TODO:  обработать добавление для несуществующего пользователя
+        except IntegrityError as err:
+            if "categories.name" in str(err):
+                raise ValueError("Категория с таким именем уже существует")
+            else:
+                log.error("Error caused in add_one", exc_info=False)
+                raise err
