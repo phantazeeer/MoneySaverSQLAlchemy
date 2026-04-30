066d7ad243882cfbde361add947adb829bbc99d6
phantazeeer <shturmovik729@gmail.com>
2026-04-30 16:25:00 +0300

    feat(logging)

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import AsyncGenerator
 from app.config import settings
+from app.utils.logger import get_logger
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
+log = get_logger(__name__)
+
 engine = create_async_engine(settings.database_url)
-print(f"Connecting '{settings.database_url}'")
+log.info("Connecting '%s'", settings.database_url)
 session_factory = async_sessionmaker(engine, class_=AsyncSession)
 
 async def get_async_session() -> AsyncGenerator[AsyncSession]:
--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from app import create_app
 import uvicorn
+import os
+import yaml
 from app.config import settings
 
 app = create_app()
 
+path_to_execute = os.path.dirname(os.path.abspath(__file__))
+log_config = yaml.safe_load(path_to_execute + "/log_config.yaml")
+
 if __name__ == "__main__":
-    uvicorn.run("server:app", host=settings.API_HOST)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, log_config=log_config)
