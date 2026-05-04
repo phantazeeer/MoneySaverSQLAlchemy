65a6333dbfec62b9cc9272af2ccbe470eafb896d
phantazeeer <shturmovik729@gmail.com>
2026-05-04 14:10:54 +0300

    feat(docker compose): added healthcheck

--- app/db/database.py
+++ app/db/database.py
@@ -1,5 +1,6 @@
 from typing import AsyncGenerator
 
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.config import settings
@@ -15,3 +16,8 @@ session_factory = async_sessionmaker(engine, class_=AsyncSession)
 async def get_async_session() -> AsyncGenerator[AsyncSession]:
     async with session_factory() as session:
         yield session
+
+
+async def check_db_connection():
+    async with session_factory() as session:
+        return (await session.execute(text("SELECT 1"))).scalar_one()
