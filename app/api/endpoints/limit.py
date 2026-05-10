b9f1139a41d5d2ab893208774810c7d64bf26e38
phantazeeer <shturmovik729@gmail.com>
2026-05-10 16:42:17 +0300

    feat(limit): changed limit endpoint for new UX

--- app/api/endpoints/limit.py
+++ app/api/endpoints/limit.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Form, HTTPException
+from fastapi import APIRouter, Depends, Form, HTTPException, Path
 
 from app.api.schemas import CreateLimit, Limit
 from app.services.limits_service import LimitService
@@ -12,13 +12,14 @@ router = APIRouter(prefix="/limits", tags=["Working with limits"])
 log = get_logger(__name__)
 
 
-@router.get("/")
+@router.get("/{id}")
 async def get_user_limit(
+    limit_id: Annotated[int, Path()],
     user_id: Annotated[int, Depends(get_jwt_payload)],
     service: Annotated[LimitService, Depends(get_service)],
 ) -> Limit | None:
     try:
-        res = await service.get_limit(user_id)
+        res = await service.get_limit(user_id, id=limit_id)
         return res
     except Exception as err:
         if str(err) == "У пользователя нет лимита":
@@ -34,21 +35,22 @@ async def create_user_limit(
     service: Annotated[LimitService, Depends(get_service)],
 ):
     try:
-        await service.create_limit(user_id, limit.period, limit.value)
+        await service.create_limit(user_id, limit)
         return {"detail": "ok"}
     except Exception:
         raise
 
 
-@router.delete("/")
+@router.delete("/{id}")
 async def delete_user_limit(
+    limit_id: Annotated[int, Path()],
     user_id: Annotated[int, Depends(get_jwt_payload)],
     service: Annotated[LimitService, Depends(get_service)],
 ):
     try:
-        await service.delete_limit(user_id)
+        await service.delete_limit_by_id(user_id, limit_id)
     except Exception as err:
-        if str(err) == "У пользователя нет лимита":
-            raise HTTPException(400, "У пользователя нет лимита") from None
+        if str(err) == "Такого лимита не существует":
+            raise HTTPException(400, "Такого лимита не существует") from None
         else:
             raise
