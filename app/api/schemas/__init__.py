c096763247be4815f63ddaf25c6bbd5b018b57a3
phantazeeer <shturmovik729@gmail.com>
2026-06-06 17:32:52 +0300

    feat(excel): added filtering to export

--- app/api/schemas/__init__.py
+++ app/api/schemas/__init__.py
@@ -1,4 +1,4 @@
 from .category import Category
 from .costs_and_earnings import AddRecord, ChangeRecord, Record
 from .limit import CreateLimit, Limit
-from .user import User, UserChange, UserLogin, UserRegister
+from .user import ExportFilter, User, UserChange, UserLogin, UserRegister
